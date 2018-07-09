ddebe8822bc4152c80ceb3386d7797ef593ce33b
Avinash jain <jainavinash881@gmail.com>
2018-07-09 16:11:20 +0530

    Add files via upload

---
+++ FIZBUZZ.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+
+    for(int i =1; i<=100;i++)
+	{
+   
+     
+        
+        if(i%15==0)
+   
+	     printf("FIzzBuzz ",i);
+
+
+	        
+        else if(i%3 == 0)
+  
+		printf("Fizz  ");
+
+
+	        
+        else if(i%5==0)	
+                                printf("BUzz ");
+
+
+        
+      		  else
+		
+        printf("%d  ",i);
+
+      	  
+    }
+
+
+    return 0;
+
+}
